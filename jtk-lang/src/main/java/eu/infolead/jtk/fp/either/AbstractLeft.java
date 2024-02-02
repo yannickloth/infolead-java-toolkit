1668f264565f4e18892dfa95acffb708bf58ccc2
Yannick Loth <727881+yannickloth@users.noreply.github.com>
2024-02-02 10:20:02 +0100

    fix javadoc

--- jtk-lang/src/main/java/eu/infolead/jtk/fp/either/AbstractLeft.java
+++ jtk-lang/src/main/java/eu/infolead/jtk/fp/either/AbstractLeft.java
@@ -9,8 +9,8 @@ import eu.infolead.jtk.logic.Bool;
 sealed abstract class AbstractLeft<L, R> extends AbstractEither<L, R, L>
         permits Left, Validation.Invalid, Maybe.None, Result.Failure {
     /**
-     * The non-serializable super class of a "Serializable" class (e.g. {@link Some}
-     * and {@link None}) should have a no-argument constructor
+     * The non-serializable super class of a "Serializable" class (e.g. {@link eu.infolead.jtk.fp.either.Maybe.Some}
+     * and {@link eu.infolead.jtk.fp.either.Maybe.None}) should have a no-argument constructor
      * <p>
      * Cf. <a target="_blank" href=
      * "https://rules.sonarsource.com/java/RSPEC-2055">https://rules.sonarsource.com/java/RSPEC-2055</a>
